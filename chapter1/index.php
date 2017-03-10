778b053899beb07e455ec4986b8f2d60568a4699
Goran <goran.dam@gmail.com>
2017-03-10 19:23:21 +0100

    Courier instantiate

---
+++ chapter1/index.php
@@ -0,0 +1,12 @@
+<?php
+
+
+function __autoload($classname) {
+include strtolower($classname) . '.php';
+}
+
+$mono = new Courier('Monospace Delivery');
+$dono =  new Courier('Douspace Delivery');
+
+var_dump($mono);
+var_dump($dono);
