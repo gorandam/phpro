0b7d180c8ef9ff03719b80f656c21bdfd86d125e
Goran <goran.dam@gmail.com>
2017-03-21 20:35:37 +0100

    Magic methods

--- oopcourse/theory/index.php
+++ oopcourse/theory/index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 class User{
 	private $id;
 	private $username;
@@ -29,9 +30,32 @@ class User{
 	
 }
 
-$User = new User('brad', '1234');
-echo $User->username;
+//$User = new User('brad', '1234');
+//echo $User->username;
 
 //$User->register();
 //$User->login();
+
+
+class Post{
+	private $name;
+	
+	public function __set($name, $value) {
+		echo 'Setting '.$name. ' to <strong>' .$value. '</strong><br />';
+		$this->name = $value;
+	}
+	public function __get($name) {
+		echo 'Getting ' .$name.' <strong>'. $this->name . '</strong><br />';
+	}
+	public function __isset($name) {
+		echo 'Is '. $name. 'set?<br />';
+		return isset($this->name);
+	}
+	
+}
+
+$post = new Post;
+$post->name = 'Testing';
+echo $post->name;
+var_dump(isset($post->name));
 
