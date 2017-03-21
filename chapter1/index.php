f49a3096ab229264490d1a68e8e9ac3bb9986385
Goran <goran.dam@gmail.com>
2017-03-21 17:21:34 +0100

    Objects and references

--- chapter1/index.php
+++ chapter1/index.php
@@ -12,7 +12,7 @@ $dono =  new Courier('Douspace Delivery');
 var_dump($mono);
 var_dump($dono);
 
-*/
+
 
 $courier = new PigeonPost('Local Avian Delivry LTD');
 
@@ -25,3 +25,26 @@ if($courier instanceOf PigeonPost) {
 	echo $courier->name . " is a PigeonPost\n";
 }
 
+*/
+
+
+
+
+$box1 = new Parcel();
+$box1->destinationCountry = 'Denmark';
+
+$box2 = $box1;
+$box2->destinationCountry = 'Brazil';
+
+echo 'Parcels need to ship to: '
+	. $box1->destinationCountry . ' and '
+	. $box2->destinationCountry;
+
+
+if ($box1 == $box2) {
+	echo 'equivalent \n';
+}
+
+if ($box1 === $box2) {
+	echo 'exact same object \n';
+}
