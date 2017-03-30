6b0ca51bc336dd08968b3487ffe7382a52b0bbf8
Goran <goran.dam@gmail.com>
2017-03-30 17:09:05 +0200

    Fetchig data

--- oopcourse/theory/oopanddatabases/pdoclass/index.php
+++ oopcourse/theory/oopanddatabases/pdoclass/index.php
@@ -4,3 +4,22 @@ require 'classes/Database.php';
 
 $database = new Database();
 //var_dump($database);
+
+$database->query('SELECT * FROM posts WHERE id = :id');
+$database->bind(':id', 2);
+$rows = $database->resultset();
+//print_r($rows);
+
+?>
+<h1>Posts<h1>
+<div>
+	<?php foreach($rows as $row) : ?>
+	<div>
+		<h3><?php echo $row['title']; ?></h3>
+		<p><?php echo $row['body']; ?></p>
+	
+	</div>
+<?php endforeach; ?>
+	
+</div>
+	
